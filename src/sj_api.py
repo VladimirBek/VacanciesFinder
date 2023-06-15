466253a1ebf9c37323476a84a3619d0514915bbb
VladimirBek <vladimirbek1996@gmail.com>
2023-06-15 11:02:22 +0300

    added docs in SuperJobAPI class method

--- src/sj_api.py
+++ src/sj_api.py
@@ -15,11 +15,12 @@ class SuperJobAPI(API):
         self._headers = {'X-Api-App-Id': os.getenv('SJ_API_KEY')}
 
     def get_vacancies(self, profession: str) -> list:
+        """
+        Метод для получения списка вакансий по ключевым словам
+        :param profession: Ключевое слово для поиска вакансий (наименование профессии)
+        :return: список вакансий
+        """
         req = requests.get(self._url, headers=self._headers,
                            params={"count": 100, "archive": False, 'keyword': profession})
         data = req.json()
         return data['objects']
-
-
-sj_api = SuperJobAPI()
-print(sj_api.get_vacancies('HR'))
