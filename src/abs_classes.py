67d1aa87cb1a2aad55284e9a28de25bdbe389cf4
VladimirBek <vladimirbek1996@gmail.com>
2023-06-17 13:27:19 +0300

    added abs class for JSON classes

--- src/abs_classes.py
+++ src/abs_classes.py
@@ -12,3 +12,24 @@ class API(ABC):
         Абстрактный метод для получения объекта для работы с API
         """
         pass
+
+
+class JSON(ABC):
+    """
+    Абстрактный метод для работы с файлами JSON формата
+    """
+
+    @abstractmethod
+    def add_vacancy(self, id):
+        """Метод для добавления вакансии в файл"""
+        pass
+
+    @abstractmethod
+    def del_vacancy(self, id):
+        """Метод для удаления вакансии из файла"""
+        pass
+
+    @abstractmethod
+    def get_formate_vacancies(self, id):
+        """Метод для получения вакансий из файла в формате типа Vacancy"""
+        pass
